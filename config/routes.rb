1524cda246e39abb0da459a5e58c9c899366fc4d
Ayanthi Saparamadu <nadeeshaddna@gmail.com>
2023-05-22 15:08:24 +1000

    compeleted

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #CREATE
+  get  "tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+
+  # READ
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #EDIT
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id",to: "tasks#update"
+
+  #DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
